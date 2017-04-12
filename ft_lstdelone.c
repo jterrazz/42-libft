7f0614501f40ed416d6351422b4d950f5d087bc1
Jean-baptiste TERRAZZONI <jterrazz@e3r2p11.42.fr>
2017-04-12 21:13:23 +0200

    regerg

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,13 +6,13 @@
 /*   By: jterrazz <jterrazz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/12 19:02:58 by jterrazz          #+#    #+#             */
-/*   Updated: 2017/04/12 21:11:22 by jterrazz         ###   ########.fr       */
+/*   Updated: 2017/04/12 21:12:46 by jterrazz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstdelone(t_list **alst, (*del)(void *, size_t))
+void	ft_lstdelone(t_list **alst, void (*del)(void *, size_t))
 {
 	if (!alst || !del || !*alst)
 		return ;
