0d33a754b57e9cec92320d8721835396fcf0fe4b
Jean-baptiste TERRAZZONI <jterrazz@e1r13p5.42.fr>
2017-04-13 00:11:30 +0200

    fwewef

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: jterrazz <jterrazz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/10 17:18:07 by jterrazz          #+#    #+#             */
-/*   Updated: 2017/04/12 14:54:18 by jterrazz         ###   ########.fr       */
+/*   Updated: 2017/04/13 00:10:07 by jterrazz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,15 +20,15 @@ void *ft_memccpy(void *dst, const void *src, int c, size_t n)
 	size_t i;
 
 	i = 0;
-	dst_str = (unsigned char *) dst;
-	src_str = (unsigned char *) src;
-	c_char = (unsigned char) c;
+	dst_str = (unsigned char *)dst;
+	src_str = (unsigned char *)src;
+	c_char = (unsigned char)c;
 	while (i < n && src_str[i] != c_char)
 	{
 		dst_str[i] = src_str[i];
 		i++;
 	}
-	if (src_str[i] == c_char && (i + 1 != n))
+	if (src_str[i] == c_char)
 	{
 		dst_str[i] = src_str[i];
 		return (&dst_str[i + 1]);
--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: jterrazz <jterrazz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/10 16:48:25 by jterrazz          #+#    #+#             */
-/*   Updated: 2017/04/12 14:54:34 by jterrazz         ###   ########.fr       */
+/*   Updated: 2017/04/12 23:56:24 by jterrazz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@ void *ft_memcpy(void *dst, const void *src, size_t n)
 	unsigned char *dst_str;
 	unsigned char *src_str;
 
-	dst_str = (unsigned char *) dst;
-	src_str = (unsigned char *) src;
+	dst_str = (unsigned char *)dst;
+	src_str = (unsigned char *)src;
 	while (--n + 1)
 		dst_str[n] = src_str[n];
 	return (dst);
