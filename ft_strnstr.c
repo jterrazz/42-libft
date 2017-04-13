4a8931354a0110bb5cf32af34abb1108df42654c
Jean-baptiste TERRAZZONI <jterrazz@e1r2p5.42.fr>
2017-04-13 21:01:43 +0200

    bientot blyat

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: jterrazz <jterrazz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/10 20:19:52 by jterrazz          #+#    #+#             */
-/*   Updated: 2017/04/13 20:44:22 by jterrazz         ###   ########.fr       */
+/*   Updated: 2017/04/13 21:00:19 by jterrazz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,21 @@
 
 char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
-	size_t i;
-	size_t j;
-	size_t size;
+	size_t found;
 
-	size = 0;
-	while (little[size] != '\0')
-		size++;
-	if (size == 0)
+	found = 0;
+	if (*little == 0)
 		return ((char *)big);
-	i = 0;
-	while (big[i] != '\0')
+	while (*big && len)
 	{
-		j = 0;
-		while (big[i + j] == little[j] && j < len)
-		{
-			if (little[j + 1] == '\0' || j + 1 == len)
-				return ((char *)(big + i));
-			j++;
-		}
-		i++;
+		if (*big == little[found])
+			found++;
+		else
+			found = 0;
+		if (!little[found])
+			return ((char *)big - found + 1);
+		big++;
+		len--;
 	}
 	return (NULL);
 }
