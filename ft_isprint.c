910bfd53fb50869f32122fa7c4e39fbe75665f20
Jean-baptiste TERRAZZONI <jterrazz@e1r2p5.42.fr>
2017-04-13 16:05:23 +0200

    errors

--- ft_isprint.c
+++ ft_isprint.c
@@ -6,7 +6,7 @@
 /*   By: jterrazz <jterrazz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/11 19:19:27 by jterrazz          #+#    #+#             */
-/*   Updated: 2017/04/13 13:05:20 by jterrazz         ###   ########.fr       */
+/*   Updated: 2017/04/13 16:05:07 by jterrazz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int		ft_isprint(int c)
 	unsigned char c_char;
 
 	c_char = (unsigned char)c;
-	if (c_char >= 32 && c_char <= 127)
+	if (c_char >= 32 && c_char <= 126)
 		return (1);
 	return (0);
 }
