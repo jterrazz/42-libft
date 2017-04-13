ecad700096c23143d4d5635986c937ded7d2fb98
Jean-baptiste TERRAZZONI <jterrazz@e1r2p5.42.fr>
2017-04-13 19:44:41 +0200

    fwefwef

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,38 +6,52 @@
 /*   By: jterrazz <jterrazz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/12 12:52:35 by jterrazz          #+#    #+#             */
-/*   Updated: 2017/04/12 23:21:27 by jterrazz         ###   ########.fr       */
+/*   Updated: 2017/04/13 19:44:21 by jterrazz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static unsigned int	ft_lsize(long n)
+static int			ft_lsize(long n)
 {
-	unsigned int size;
-	
+	int size;
+
 	size = 1;
 	while (n /= 10)
 		size++;
 	return (size);
 }
 
-//static void			set_itoa(char *str, long n_long, unsigned int neg, unsigned int len)
-//{
-//	int divider;
-//	
-//	divider = 1;
-//	while (--len)
-//		divider *= 10;
-//}
+static void			set_itoa(char *str, long n_long, int neg, int len)
+{
+	long	divider;
+	int		i;
+
+	i = 0;
+	divider = 1;
+	while (len--)
+		divider *= 10;
+	if (neg == 1)
+	{
+		str[i] = '-';
+		i++;
+	}
+	while (divider /= 10)
+	{
+		str[i] = n_long / divider + '0';
+		i++;
+		n_long = n_long - (n_long / divider) * divider;
+	}
+}
+
 char				*ft_itoa(int n)
 {
-	char *str;
-	long n_long;
-	unsigned int neg;
-	unsigned int len;
+	char			*str;
+	long			n_long;
+	int	neg;
+	int	len;
 
-	n_long = n;	
+	n_long = (long)n;
 	neg = 0;
 	if (n_long < 0)
 	{
@@ -48,6 +62,11 @@ char				*ft_itoa(int n)
 	str = ft_strnew(len + neg);
 	if (str == NULL)
 		return (NULL);
-//	set_itoa(str, n_long, neg, len);
+	if (n_long == 0)
+	{
+		str[0] = '0';
+		return (str);
+	}
+	set_itoa(str, n_long, neg, len);
 	return (str);
 }
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,25 +6,23 @@
 /*   By: jterrazz <jterrazz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/12 12:20:56 by jterrazz          #+#    #+#             */
-/*   Updated: 2017/04/13 13:07:36 by jterrazz         ###   ########.fr       */
+/*   Updated: 2017/04/13 19:15:48 by jterrazz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-unsigned int	get_trim_length(const char *str)
+static unsigned int		get_trim_length(const char *str)
 {
-	unsigned int i;
-	unsigned int end;
+	int i;
+	int end;
 
 	end = ft_strlen(str) - 1;
 	i = 0;
 	while (str[i] && (str[i] == ' ' || str[i] == '\n' || str[i] == '\t'))
 		i++;
-	while (str[end] == ' ' || str[end] == '\n' || str[end] == '\t')
+	while (end >= 0 && (str[end] == ' ' || str[end] == '\n' || str[end] == '\t'))
 	{
-		if (end == 0)
-			break;
 		end--;
 	}
 	if (end - i + 1 <= 0)
@@ -32,7 +30,7 @@ unsigned int	get_trim_length(const char *str)
 	return (end - i + 1);
 }
 
-char			*ft_strtrim(char const *s)
+char					*ft_strtrim(char const *s)
 {
 	char			*new_s;
 	unsigned int	len;
