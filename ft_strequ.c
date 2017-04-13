8418cae7763d2a85bc8e67d9e381ee6a2815cd64
Jean-baptiste TERRAZZONI <jterrazz@e1r2p5.42.fr>
2017-04-13 18:55:17 +0200

    wef

--- ft_strequ.c
+++ ft_strequ.c
@@ -6,7 +6,7 @@
 /*   By: jterrazz <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/13 13:20:31 by jterrazz          #+#    #+#             */
-/*   Updated: 2017/04/13 13:26:18 by jterrazz         ###   ########.fr       */
+/*   Updated: 2017/04/13 18:55:02 by jterrazz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@ int		ft_strequ(char const *s1, char const *s2)
 	int i;
 
 	i = 0;
-	while (s1[i] == s2[i])
+	while (s1[i] == s2[i] && s1[i] && s2[i])
 		i++;
 	if (!s1[i] && !s2[i])
 		return (1);
