e52e03e16a0bc96fc245068de07806306ca48526
Jean-baptiste TERRAZZONI <jterrazz@e1r2p5.42.fr>
2017-04-15 01:12:29 +0200

    wfwef

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: jterrazz <jterrazz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/10 16:48:25 by jterrazz          #+#    #+#             */
-/*   Updated: 2017/04/15 01:10:28 by jterrazz         ###   ########.fr       */
+/*   Updated: 2017/04/15 01:12:18 by jterrazz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	unsigned char *dst_str;
-	unsigned char *src_str;
-	size_t i;
+	unsigned char	*dst_str;
+	unsigned char	*src_str;
+	size_t			i;
 
 	i = 0;
 	dst_str = (unsigned char *)dst;
