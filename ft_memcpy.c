a264cdac096f99e7d600fef00134e648e12d39cf
Jean-baptiste TERRAZZONI <jterrazz@e1r2p5.42.fr>
2017-04-15 01:11:05 +0200

    efw

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: jterrazz <jterrazz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/10 16:48:25 by jterrazz          #+#    #+#             */
-/*   Updated: 2017/04/14 20:38:31 by jterrazz         ###   ########.fr       */
+/*   Updated: 2017/04/15 01:10:28 by jterrazz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,15 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
 	unsigned char *dst_str;
 	unsigned char *src_str;
+	size_t i;
 
+	i = 0;
 	dst_str = (unsigned char *)dst;
 	src_str = (unsigned char *)src;
-	while (--n + 1)
-		dst_str[n] = src_str[n];
+	while (i < n)
+	{
+		dst_str[i] = src_str[i];
+		i++;
+	}
 	return (dst);
 }
--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: jterrazz <jterrazz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/10 18:26:16 by jterrazz          #+#    #+#             */
-/*   Updated: 2017/04/13 12:58:02 by jterrazz         ###   ########.fr       */
+/*   Updated: 2017/04/15 01:04:06 by jterrazz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,25 +16,19 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 {
 	unsigned char	*dst_str;
 	unsigned char	*src_str;
-	unsigned char	*tmp;
 	size_t			i;
 
 	i = 0;
 	dst_str = (unsigned char *)dst;
 	src_str = (unsigned char *)src;
-	tmp = (unsigned char *)malloc(sizeof(unsigned char) * len);
-	if (tmp == NULL)
-		return (dst);
-	while (i < len)
+	if (dst_str < src_str)
+		ft_memcpy(dst_str, src_str, len);
+	else
 	{
-		tmp[i] = src_str[i];
-		i++;
-	}
-	i = 0;
-	while (i < len)
-	{
-		dst_str[i] = *tmp++;
-		i++;
+		while (len--)
+		{
+			dst_str[len] = src_str[len];
+		}
 	}
 	return (dst);
 }
